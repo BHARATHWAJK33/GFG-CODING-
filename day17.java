45ab492689e5dc40179206c7831adbdc532d8dc8
BHARATHWAJ K <bharathwaj6366@gmail.com>
2025-04-22 07:45:51 +0530

    Create day17.java

---
+++ day17.java
@@ -0,0 +1,18 @@
+class Solution {
+    static char nonRepeatingChar(String s) {
+        // code here
+       int n = s.length();
+        for (int i = 0; i < n; ++i) {
+            boolean found = false;
+            for (int j = 0; j < n; ++j) {
+                if (i != j && s.charAt(i) == s.charAt(j)) {
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) 
+                return s.charAt(i);
+        }
+        return '$';
+    }
+}
