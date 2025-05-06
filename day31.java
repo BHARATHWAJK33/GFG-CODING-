722f3143a919e67474655b421714e29838d06455
BHARATHWAJ K <bharathwaj6366@gmail.com>
2025-05-06 19:41:23 +0530

    Create day31.java

---
+++ day31.java
@@ -0,0 +1,20 @@
+class Solution {
+
+    public int peakElement(int[] arr) {
+        // code here
+        int n = arr.length;
+
+        for (int i = 0; i < n; i++) {
+            boolean left = true;
+            boolean right = true;
+            if (i > 0 && arr[i] <= arr[i - 1])
+                left = false;
+            if (i < n - 1 && arr[i] <= arr[i + 1])
+                right = false;
+            if (left && right) {
+                return i;
+            }
+        }
+        return 0;
+    }
+}
