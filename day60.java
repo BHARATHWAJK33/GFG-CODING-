1d7cf437330769a6f31f42a4b18d5e65545ac781
BHARATHWAJ K <bharathwaj6366@gmail.com>
2025-06-04 08:17:21 +0530

    Create day60.java

---
+++ day60.java
@@ -0,0 +1,25 @@
+class Solution {
+
+    public int maxWater(int height[]) {
+        // Code Here
+         int left = 0;
+        int right = height.length - 1;
+        int maxArea = 0;
+
+        while (left < right) {
+            int h = Math.min(height[left], height[right]);
+            int w = right - left;
+            int area = h * w;
+            maxArea = Math.max(maxArea, area);
+
+            // Move the shorter line inward
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return maxArea;
+    }
+}
